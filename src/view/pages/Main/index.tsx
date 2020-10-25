78db2d091eb5f379289f70f1dd7d8f790a06a006
Gaisar Davletkildin <gais.davletkildin@gmail.com>
2020-10-25 22:13:07 +0300

    Маленькие правки стилей

--- src/view/pages/Main/index.tsx
+++ src/view/pages/Main/index.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from "react";
 import { observer } from "mobx-react-lite";
 
-import Card from "../../common/components/Card";
 import styles from "./styles.module.scss";
+import Card from "../../common/components/Card";
 import Subtitle from "../../common/components/Subtitle";
 import Calendar from "./components/Calendar";
 
@@ -11,22 +11,28 @@ interface IProps {
 }
 
 const Main: FC<IProps> = ({ calendarApi }) => {
+  const { card, balance, calendarWrapper } = styles;
+
   return (
     <>
-      <Card className={styles.card}>
+      <Card className={card}>
         <Subtitle>Лимит на месяц: 20 000 руб.</Subtitle>
       </Card>
-      <Card className={styles.card}>
-        <div className={styles.balance}>Остаток на месяц: 15 500 руб.</div>
+      <Card className={card}>
+        <div className={balance}>Остаток на месяц: 15 500 руб.</div>
         <div>Дневной остаток: 700 руб.</div>
       </Card>
-      <Card className={styles.card}>
-        <div className={styles.balance}>Остаток на месяц: 15 500 руб.</div>
-        <div className={styles.balance}>Остаток на месяц: 15 500 руб.</div>
+      <Card className={card}>
+        <div className={balance}>Остаток на месяц: 15 500 руб.</div>
+        <div className={balance}>Остаток на месяц: 15 500 руб.</div>
         <div>Дневной остаток: 700 руб.</div>
       </Card>
-      <Card className={styles.calendarWrapper}>
+      <Card className={[card, calendarWrapper]}>
         <Calendar calendarApi={calendarApi}>Calendar</Calendar>
+      </Card>
+      <Card className={card}>
+        <div className={balance}>Остаток на месяц: 15 500 руб.</div>
+        <div>Дневной остаток: 700 руб.</div>
       </Card>
     </>
   );
