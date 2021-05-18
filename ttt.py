bb61ff70698687169bce4c6ba40829b5a27682e7
Gaisar Davletkildin <gais.davletkildin@gmail.com>
2021-05-18 21:03:36 +0300

    профиль

--- ttt.py
+++
@@ -1,9 +0,0 @@
-numbers = [5, 2, 5, 2, 2]
-
-for symbol in numbers: # первый цикл
-    words = ''
-
-    for symbol_one in range(symbol): # второй цикл
-        words = words + '*'
-
-    print(words)
